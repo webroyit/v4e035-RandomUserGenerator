1b4209036c5b61be4c01c623ad82f7033e4624d2
royweb <xytank@gmail.com>
2021-04-05 15:35:33 -0400

    Use data() to display user account

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const app = Vue.createApp({
-    template: '<h1>Random User Generator</h1>'
+    // data() is a function that must be return
+    data() {
+        return {
+            firstName: 'Bob',
+            lastName: 'Fair',
+            email: 'bobfair123abc@mail.com',
+            gender: 'male',
+            picture: 'https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+        }
+    }
 });
 
 app.mount('#app');
