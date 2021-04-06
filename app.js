1e966ea3d4ff4a6e78f307418d193e2583a1d70d
royweb <xytank@gmail.com>
2021-04-06 13:21:56 -0400

    Add method to change user data when the button is clicked

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
         }
+    },
+    methods: {
+        getUser() {
+            this.firstName = 'Jill';
+            this.lastName = 'Time';
+            this.email = 'jilltime123abc@mail.com';
+            this.gender = 'female';
+            this.picture = 'https://images.unsplash.com/photo-1554555819-f722cb0c01c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80';
+        }
     }
 });
 
